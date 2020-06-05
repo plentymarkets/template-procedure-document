588e9c98d8febefcd8d3c712f1fd6337fb3e51cd
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2020-06-05 16:57:22 +0200

    fix file to convert

--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@ require 'asciidoctor'
 require 'asciidoctor-pdf'
 require 'asciidoctor-diagram'
 
-Asciidoctor.convert_file 'anhang-rest.adoc', toFile: true, backend: 'pdf', safe: :safe
+Asciidoctor.convert_file 'Index.adoc', toFile: true, backend: 'pdf', safe: :safe
